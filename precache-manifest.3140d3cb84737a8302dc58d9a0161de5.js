edfd0e0f91b8a65b278fb96d88f844a1b892fec5
Cheick Mahady SISSOKO <cheickm.sissoko@gmail.com>
2020-12-19 00:09:23 +0100

    Updates

---
+++ precache-manifest.3140d3cb84737a8302dc58d9a0161de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a00da67e7596e426833d6d9532c057",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc9638bfb2ed510924cb",
+    "url": "./static/css/2.515377c4.chunk.css"
+  },
+  {
+    "revision": "6282e7a8f9e1c1ba0321",
+    "url": "./static/css/main.96e8b843.chunk.css"
+  },
+  {
+    "revision": "dc9638bfb2ed510924cb",
+    "url": "./static/js/2.d19a1431.chunk.js"
+  },
+  {
+    "revision": "d00f20e93604d4806d6d9def73bfeb93",
+    "url": "./static/js/2.d19a1431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6282e7a8f9e1c1ba0321",
+    "url": "./static/js/main.f04cfd9a.chunk.js"
+  },
+  {
+    "revision": "0c3138b471bf1b2993f1",
+    "url": "./static/js/runtime-main.5d74d3e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3f90843260d75a5022680b05e339d849.js"
+  "./precache-manifest.3140d3cb84737a8302dc58d9a0161de5.js"
 );
 
 self.addEventListener('message', (event) => {
