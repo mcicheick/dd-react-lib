b609e13605e0e56604a7db7a3f6a0fb3631acaae
Cheick Mahady SISSOKO <cheickm.sissoko@gmail.com>
2020-12-18 12:57:40 +0100

    Updates

---
+++ precache-manifest.3f90843260d75a5022680b05e339d849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e2b4d08beb57b0a4f0e7d8b7c8cd6c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "702f0919ea6a560d890b",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "8d508f4c2c435e2644a6",
+    "url": "./static/js/2.d5c3394e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d5c3394e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702f0919ea6a560d890b",
+    "url": "./static/js/main.1c71c71c.chunk.js"
+  },
+  {
+    "revision": "0c3138b471bf1b2993f1",
+    "url": "./static/js/runtime-main.5d74d3e3.js"
+  }
+]);
